c81755864627ba25e324ba3adae3e68b2710f73f
Auralcat <ashleyzt@hi2.in>
2018-07-07 20:34:31 -0300

    Check if index.php is working

--- public/index.php
+++ public/index.php
@@ -2,10 +2,10 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 $app = new \Slim\App;
-$app->get('/hello/{name}', function(Request $request, Response $response {
+$app->get('/hello/{name}', function(Request $request, Response $response) {
             $name = $request->getAttribute('name');
             $response->getBody()->write("Hello, $name");
 
