5f55ec9e18bf686124bfb4e5b3e7c2bf2213bfc4
Auralcat <ashleyzt@hi2.in>
2018-07-08 12:25:34 -0300

    Add Monolog as dependency

--- public/index.php
+++ public/index.php
@@ -2,11 +2,23 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 require '../vendor/autoload.php';
 require '../src/config/db.php';
 
 /* Call the objects in the client using host/slimapp/index.php/hello/whatever */
 $app = new \Slim\App;
+
+// Create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('../logs/error.log', Logger::WARNING));
+
+// Add records to the log
+$log->warning('Foo');
+$log->error('Bar');
+
 $app->get('/hello/{name}', function(Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
