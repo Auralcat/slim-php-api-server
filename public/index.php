5ca3cd17b69f8099cfac845b2ef856c090f9ae11
Auralcat <ashleyzt@hi2.in>
2018-07-08 00:00:54 -0300

    Link db.php to index.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
-
+require '../src/config/db.php';
 
 /* Call the objects in the client using host/slimapp/index.php/hello/whatever */
 $app = new \Slim\App;
