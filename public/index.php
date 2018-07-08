1ddabb31367328c86d59fd6fe497d2f84772d7af
Auralcat <ashleyzt@hi2.in>
2018-07-07 21:30:19 -0300

    Create .htaccess and customers.php

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
+/* Call the objects in the client using host/slimapp/index.php/hello/whatever */
 $app = new \Slim\App;
 $app->get('/hello/{name}', function(Request $request, Response $response) {
             $name = $request->getAttribute('name');
@@ -12,4 +13,7 @@ $app->get('/hello/{name}', function(Request $request, Response $response) {
             return $response;
         });
     $app->run();
+
+// Customer Routes
+require '../src/routes/customers.php'
 ?>
