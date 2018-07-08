5c4617030a10fc8f08817040016a07470f3fc6b7
Auralcat <ashleyzt@hi2.in>
2018-07-07 23:56:09 -0300

    Adjust customer route, create db-sample.php

--- public/index.php
+++ public/index.php
@@ -4,16 +4,18 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
+
 /* Call the objects in the client using host/slimapp/index.php/hello/whatever */
 $app = new \Slim\App;
 $app->get('/hello/{name}', function(Request $request, Response $response) {
-            $name = $request->getAttribute('name');
-            $response->getBody()->write("Hello, $name");
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
 
-            return $response;
-        });
-    $app->run();
+    return $response;
+});
 
 // Customer Routes
-require '../src/routes/customers.php'
+require '../src/routes/customers.php';
+
+$app->run();
 ?>
---
+++ src/config/db-sample.php
@@ -0,0 +1,11 @@
+<?php
+// Here's where our DB connection params go.
+
+class db
+{
+    private $dbhost = 'localhost';
+    private $dbuser = 'your_user_goes_here';
+    private $dbpass = 'your_pass_goes_here';
+    private $dbname = 'slimapp';
+}
+?>
