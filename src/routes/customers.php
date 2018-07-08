e20c1e92273da9e1ab03aa6ac8f10e734e51ae2b
Auralcat <ashleyzt@hi2.in>
2018-07-08 00:11:53 -0300

    Connect database and API endpoint

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -6,5 +6,21 @@ $app = new \Slim\App;
 
 // Get All Customers
 $app->get('/api/customers', function(Request $request, Response $response) {
-    echo "I'm calling from " . shell_exec("pwd");
+    $sql = "SELECT * FROM customers";
+    try
+    {
+        // Get DB object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $customers = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        // Return the result as a JSON array.
+        echo json_encode($customers);
+    } catch(PDOException $e)
+    {
+        echo '{"error": {"text": ' . $e->getMessage() . '}';
+    }
 });
