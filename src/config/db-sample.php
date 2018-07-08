e1e38fdb47416201ecc2efb187c896ba36407449
Auralcat <ashleyzt@hi2.in>
2018-07-07 23:59:25 -0300

    Complete db-sample.php

--- src/config/db-sample.php
+++ src/config/db-sample.php
@@ -7,5 +7,13 @@ class db
     private $dbuser = 'your_user_goes_here';
     private $dbpass = 'your_pass_goes_here';
     private $dbname = 'slimapp';
+
+    public function connect()
+    {
+        $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname;"
+        $dbConnection = new PDO($mysql_connect_str, $this->dbuser, $this->dbpass);
+        $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $dbConnection;
+    }
 }
 ?>
