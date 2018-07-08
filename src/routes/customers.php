79aabf9886eb354968ed2aaa67e1d04b6302c612
Auralcat <ashleyzt@hi2.in>
2018-07-08 00:49:36 -0300

    Create update customer API endpoint

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -86,4 +86,53 @@ $app->post('/api/customer/add', function(Request $request, Response $response) {
         echo '{"error": {"text": ' . $e->getMessage() . '}';
     }
 });
+
+// Update customer details in the database
+// TODO: Why can't you send just one param in the JSON?
+// As of now, if you do that, all the other fields are set to null.
+$app->put('/api/customer/update/{id}', function(Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+
+    $first_name = $request->getParam('first_name');
+    $last_name = $request->getParam('last_name');
+    $email = $request->getParam('email');
+    $phone = $request->getParam('phone');
+    $address = $request->getParam('address');
+    $city = $request->getParam('city');
+    $state = $request->getParam('state');
+
+    $sql = "UPDATE customers SET "
+         . "first_name = :first_name, "
+         . "last_name  = :last_name, "
+         . "phone      = :phone, "
+         . "email      = :email, "
+         . "address    = :address, "
+         . "city       = :city, "
+         . "state      = :state "
+         . "WHERE id = $id";
+    try
+    {
+        // Get DB object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':first_name', $first_name);
+        $stmt->bindParam(':last_name',  $last_name);
+        $stmt->bindParam(':phone',      $phone);
+        $stmt->bindParam(':email',      $email);
+        $stmt->bindParam(':address',    $address);
+        $stmt->bindParam(':city',       $city);
+        $stmt->bindParam(':state',      $state);
+
+        $stmt->execute();
+
+        echo '{"notice": {"text": "Customer Updated"}}';
+    } catch(PDOException $e)
+    {
+        echo '{"error": {"text": ' . $e->getMessage() . '}';
+    }
+});
 ?>
