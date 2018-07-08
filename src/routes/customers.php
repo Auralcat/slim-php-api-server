2e52c87d3a76d07367edd3f56bb06506fce2fc25
Auralcat <ashleyzt@hi2.in>
2018-07-08 12:04:47 -0300

    Create delete customer API endpoint

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -2,8 +2,6 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-$app = new \Slim\App;
-
 // Get All Customers
 $app->get('/api/customers', function(Request $request, Response $response) {
     $sql = "SELECT * FROM customers";
@@ -130,6 +128,67 @@ $app->put('/api/customer/update/{id}', function(Request $request, Response $resp
         $stmt->execute();
 
         echo '{"notice": {"text": "Customer Updated"}}';
+    } catch(PDOException $e)
+    {
+        echo '{"error": {"text": ' . $e->getMessage() . '}';
+    }
+});
+
+// Delete single customer
+$app->delete('/api/customer/delete/{id}', function(Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $sql = "DELETE FROM customers WHERE id = $id";
+    try
+    {
+        // Get DB object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice": {"text": "Customer deleted"}}';
+    } catch(PDOException $e)
+    {
+        echo '{"error": {"text": ' . $e->getMessage() . '}';
+    }
+});
+
+// Add customer to database
+$app->post('/api/customer/add', function(Request $request, Response $response) {
+    // Can't you do that with an array?
+    $first_name = $request->getParam('first_name');
+    $last_name = $request->getParam('last_name');
+    $email = $request->getParam('email');
+    $phone = $request->getParam('phone');
+    $address = $request->getParam('address');
+    $city = $request->getParam('city');
+    $state = $request->getParam('state');
+    $sql = "INSERT INTO customers "
+         . "(first_name, last_name, phone, email, address, city, state) "
+         . "VALUES "
+         . "(:first_name, :last_name, :phone, :email, :address, :city, :state)";
+    try
+    {
+        // Get DB object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':first_name', $first_name);
+        $stmt->bindParam(':last_name',  $last_name);
+        $stmt->bindParam(':phone',      $phone);
+        $stmt->bindParam(':email',      $email);
+        $stmt->bindParam(':address',    $address);
+        $stmt->bindParam(':city',       $city);
+        $stmt->bindParam(':state',      $state);
+
+        $stmt->execute();
+
+        echo '{"notice": {"text": "Customer Added"}}';
     } catch(PDOException $e)
     {
         echo '{"error": {"text": ' . $e->getMessage() . '}';
