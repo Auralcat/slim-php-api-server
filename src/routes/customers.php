5dd26d8781dd8541cf0e12b47d14d84b15637244
Auralcat <ashleyzt@hi2.in>
2018-07-08 00:19:29 -0300

    Add API endpoint for single customer

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -24,3 +24,26 @@ $app->get('/api/customers', function(Request $request, Response $response) {
         echo '{"error": {"text": ' . $e->getMessage() . '}';
     }
 });
+
+// Get single customer
+$app->get('/api/customer/{id}', function(Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $sql = "SELECT * FROM customers WHERE id = $id";
+    try
+    {
+        // Get DB object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $customer = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        // Return the result as a JSON array.
+        echo json_encode($customer);
+    } catch(PDOException $e)
+    {
+        echo '{"error": {"text": ' . $e->getMessage() . '}';
+    }
+});
+?>
